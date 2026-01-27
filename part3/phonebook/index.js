b64bfcc5c3ce0135fec62a4256ee8e434dc99535
xsmh <sh@smh.sh>
2026-01-27 22:09:40 +0300

    Add cors

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
 const port = 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
 let persons = [
