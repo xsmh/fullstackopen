12e910df17691aa8f1a95c5d5f13907f7229b909
xsmh <sh@smh.sh>
2026-02-17 09:36:17 +0300

    Add dotenv & add mongo db to backend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,78 +1,52 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const Person = require("./models/person");
+
 const app = express();
-const port = 3001;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(express.static('public'))
-let persons = [
-  {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
 
 app.get("/api/persons", (req, res) => {
-  res.send(persons);
-})
+  Person.find({}).then(people => {
+    res.json(people);
+  });
+});
 
 app.get("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  const person = persons.find(person => person.id === id)
-  if (person) {
+  Person.findById(id).then(person => {
     res.json(person);
-  } else {
-    res.status(404).end();
-  }
-})
+  });
+});
 
 app.get("/info", (req, res) => {
   const message = `Phonebook has info for ${data.length} people`;
   const date = new Date().toString();
   res.send(`${message}<br>${date}`);
-})
+});
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
   if (!body.name || !body.number) {
     return res.status(400).json({
       error: "name or number missing"
-    })
+    });
+  }
 
-  }
-  if (persons.some(person => person.name === body.name)) {
-    return res.status(400).json({
-      error: "name must be unique"
-    })
-  }
-  const personId = Math.floor((Math.random() * 100000000000) + 1);;
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: String(personId)
-  }
+  });
 
-  persons = persons.concat(person);
-  res.json(person);
-})
+  person.save().then(savedPerson => {
+    res.json(savedPerson);
+  });
+});
 
 
 app.delete("/api/persons/:id", (req, res) => {
