e385675e5d6817196eb134b52e7aa4cbc43fa396
xsmh <sh@smh.sh>
2026-04-17 09:04:26 +0300

    Add endpoint to get all users

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -2,6 +2,12 @@ const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
 const User = require("../models/user");
 
+usersRouter.get("/", (request, response) => {
+  User.find({}).then((users) => {
+    response.json(users);
+  });
+});
+
 usersRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
 
