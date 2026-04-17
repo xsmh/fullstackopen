c4e2c468a01960c5e39a6cb330125f2b21cc734d
xsmh <sh@smh.sh>
2026-04-17 08:56:44 +0300

    Add user creation

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,18 +3,21 @@ const mongoose = require("mongoose");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 const blogsRouter = require("./controllers/blogs.js");
+const usersRouter = require("./controllers/users");
 
 const app = express();
 
 logger.info("connecting to", config.MONGODB_URI);
 
-mongoose.connect(config.MONGODB_URI, { family: 4 })
-.then(() => logger.info("connected to MongoDB"))
-.catch((error) => {
-    logger.error("error connecting to MongoDB", error.message)
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => logger.info("connected to MongoDB"))
+  .catch((error) => {
+    logger.error("error connecting to MongoDB", error.message);
   });
 
-app.use(express.json())
+app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
